212a9c63a73adfdce14514885eb8c97a2eb42498
Leo <leonid.buneev@tirecheck.cz>
2018-11-13 15:59:04 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# pug-to-html
+pug-to-html
+===========
+
 CLI tool to convert your pug code to html code. Supports Vue Single File Components!
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/pug-to-html.svg)](https://npmjs.org/package/pug-to-html)
+[![Downloads/week](https://img.shields.io/npm/dw/pug-to-html.svg)](https://npmjs.org/package/pug-to-html)
+[![License](https://img.shields.io/npm/l/pug-to-html.svg)](https://github.com/leo-buneev/pug-to-html/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ src/index.js
@@ -0,0 +1,68 @@
+const {Command, flags} = require('@oclif/command')
+const fs = require('fs-extra')
+const walk = require('klaw-sync')
+const pug = require('pug')
+const dedent = require('dedent')
+
+class PugToHtmlCommand extends Command {
+  async run() {
+    const {args} = this.parse(PugToHtmlCommand)
+
+    const cwd = args.path || process.cwd()
+    const files = walk(cwd, {
+      nodir: true,
+    })
+    let oldLength = 0
+    let newLength = 0
+    let filesCount = 0
+    for (const file of files) {
+      if (file.path.toLowerCase().endsWith('.vue')) {
+        try {
+          const fileContents = await fs.readFile(file.path, 'utf-8')
+          const newFileContents = fileContents.replace(/<template lang=['"]pug["']>\n+([\s\S]*)<\/template>/i, (substr, match) => {
+            if (match.startsWith(' ')) {
+              match = dedent(match)
+            }
+            match = match.trim()
+            let html = pug.render(match, {pretty: '  '})
+            html = html.replace(/&amp;/g, '&')
+            // &quot; is ", but it appears only inside attributes - and html attributes already use " for enclosing
+            html = html.replace(/&quot;/g, "'")
+            html = html.replace(/&gt;/g, '>')
+            html = html.replace(/&lt;/g, '<')
+            html = html.replace(/v-else="v-else"/g, 'v-else')
+            return `<template>
+${html}
+</template>`
+          })
+          await fs.writeFile(file.path, newFileContents)
+          filesCount++
+          oldLength += fileContents.length
+          newLength += newFileContents.length
+        } catch (error) {
+          this.error(`ERROR while processing ${file.path}`)
+          throw error
+        }
+      }
+    }
+    this.log(`Processed ${filesCount} files. Old length: ${oldLength}, new length: ${newLength}`)
+  }
+}
+
+PugToHtmlCommand.description = `Converts your pug code to html code.
+This tool will recursively scan provided path, and replace pug code with html code.
+
+- For *.pug files, it will replace entire file contents and rename it to *.html
+- For *.vue files, it will find <template lang="pug"> sections and replace their contents with HTML
+`
+
+PugToHtmlCommand.flags = {
+  version: flags.version({char: 'v'}),
+  help: flags.help({char: 'h'}),
+}
+
+PugToHtmlCommand.args = [
+  {name: 'path', description: 'Path to the directory where with vue files. Defaults to current working directory'},
+]
+
+module.exports = PugToHtmlCommand
